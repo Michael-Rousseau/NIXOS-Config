975b3b1d07d26df561262c8f2fbec09f443670a5
michael rousseau <michael.rousseau@epita.fr>
2023-10-04 19:04:52 +0200

    d

--- configuration.nix
+++ configuration.nix
@@ -117,16 +117,29 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    vimHugeX
     gcc
     binutils
     wget
     brightnessctl
-    vim
     neovim
     pulseaudio
     xclip
-    
+    git
+    alacritty
+    gnumake
+    discord
+    pkg-config
+    gdb
+    openssh
+    nettools
+    python3
+    libtool 
+    cmake
+    unzip
+    curl
+    zathura
+    jetbrains.pycharm-community
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
